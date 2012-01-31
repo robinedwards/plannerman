879ebf50f48206caf8ba1f3d9f3e4e805920981a
Robin Edwards <robin.ge@gmail.com>
2012-01-31 12:46:12 +0000

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlannerMan::Application.config.secret_token = 'f5ca1cd01fa76b8de560d5735ef203206324e565a19cc5f2bf043a32f79c4b08920eb650a2bb350747b380db1a09371d00c37f13f34128c632b250caedd00378'
