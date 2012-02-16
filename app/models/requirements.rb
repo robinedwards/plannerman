672ac40d48da74fad369f27eb29278c3dec49779
Robin Edwards <robin.ge@gmail.com>
2012-02-16 11:48:54 +0000

    removed legacy model file

--- app/models/requirements.rb
+++
@@ -1,5 +0,0 @@
-class Requirement < ActiveRecord::Base
-  validates :name,    :presence => true
-  has_many  :domain_requirements
-  has_many  :domains, :class_name => 'Domain', :through  => :domain_requirements
-end
